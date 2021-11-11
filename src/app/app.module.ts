1c0245c7cd09c24aaaa100600997fde6aae31225
abnerwerley <abnerwerley77@gmail.com>
2021-11-11 16:46:19 -0300

    [@abnerwerley task: methods 'entrar' and 'cadastrar' created at auth.service]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import{ HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -19,8 +20,8 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { Usuario } from '../model/Usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  entrar(credenciais: CredenciaisDTO): Observable<CredenciaisDTO> {
+    return this.http.put<CredenciaisDTO>(
+      'https://blogpessoalabner.herokuapp.com/api/v1/usuario/logar',
+      credenciais
+    );
+  }
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>(
+      'https://blogpessoalabner.herokuapp.com/api/v1/usuario/cadastrar',
+      usuario
+    );
+  }
 }

fd819c16acb6ab981c96bf78697892c05eac0200
abnerwerley <abnerwerley77@gmail.com>
2021-11-11 22:42:39 -0300

    [@abnerwerley task: method 'entrar' created, linked, and others done]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'entrar', pathMatch: 'full' }, // quando o endereço digitado for vazio, redirecione para o componente entrar
 
-  { path: 'entrar', component: EntrarComponent},
+  { path: 'entrar', component: EntrarComponent },
   { path: 'cadastrar', component: CadastrarComponent },
+  { path: 'inicio', component: InicioComponent },
 ];
 
 @NgModule({
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { AuthService } from './../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
-  styleUrls: ['./entrar.component.css']
+  styleUrls: ['./entrar.component.css'],
 })
 export class EntrarComponent implements OnInit {
+  usuarioLogin: CredenciaisDTO = new CredenciaisDTO();
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0, 0);
   }
 
+  entrar() {
+    this.auth.entrar(this.usuarioLogin).subscribe(
+      (resp: CredenciaisDTO) => {
+        this.usuarioLogin = resp;
+        environment.token = this.usuarioLogin.token;
+        environment.idUsuario = this.usuarioLogin.idUsuario;
+        environment.nome = this.usuarioLogin.nome;
+        environment.email = this.usuarioLogin.email;
+        environment.tipo = this.usuarioLogin.tipo;
+        environment.foto = this.usuarioLogin.foto;
+
+        console.log(environment);
+        this.router.navigate(['/inicio']);
+      },
+      (erro) => {
+        if (erro.status == 400) {
+          alert('Usuário ou senha inválidos.');
+        }
+      })
+  }
 }

9a1a7182a5da966e229129ce0f3078fb549ee83d
abnerwerley <abnerwerley77@gmail.com>
2021-11-16 12:31:51 -0300

    [@abnerwerley task: component 'tema' pre-configured]

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    if (environment.token == '') {
+      // alert('Sua sessão expirou, faça login novamente.');
+      this.router.navigate(['/entrar']);
+    }
   }
-
 }
